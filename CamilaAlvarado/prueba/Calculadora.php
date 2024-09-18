9ea74687eedcb566ecc85ea74936ba3641a2014d
1546329G <1546329@senati.pe>
2024-09-18 11:52:36 -0500

    copia

---
+++ CamilaAlvarado/prueba/Calculadora.php
@@ -0,0 +1,16 @@
+<?php
+$num1=20;
+$num2=10;
+$suma= $num1+$num2;
+$resta=$num1-$num2;
+$multi=$num1*$num2;
+$divi=$num1/$num2;
+    echo'BIENVENIDO A LA CALCULADORA<br>';
+    echo 'Suma '.$suma.'<br>' ;
+
+    echo 'Resta '.$resta.'<br>' ;
+
+    echo 'Divición '.$divi.'<br>' ;
+
+    echo 'Multiplicación '.$multi.'<br>' ;
+?>
