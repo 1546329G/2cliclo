638b8302b18b04332e4d4cba90dbe27473175590
1546329G <1546329@senati.pe>
2024-09-18 11:57:40 -0500

    copia

---
+++ CamilaAlvarado/prueba/Ejercicio03.php
@@ -0,0 +1,11 @@
+<?php
+$Numero = 50;
+//Comentarios
+if ($Numero < 18){
+    echo'Eres menor de edad <br>';
+}else if($Numero>18 && $Numero<50){
+    echo 'Eres Mayor de Edad <br>';
+}else
+    echo'Adulto mayor <br>';
+    
+?>
