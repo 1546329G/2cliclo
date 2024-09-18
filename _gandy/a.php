e889571b1ca590609ba410292ea8d23ec5da6dc1
1546329G <1546329@senati.pe>
2024-09-18 10:20:11 -0500

    Add files via upload

---
+++ _gandy/a.php
@@ -0,0 +1,18 @@
+<?php
+    $nombre=$_GET['nombre'];
+    $apellidos=$_GET['apellidos'];
+    $sexo=$_GET['sexo'];
+    $correo=$_GET['correo'];
+    $poblacion=$_GET['poblacion'];
+   
+
+
+    echo "Nombre: " $nombre;
+    echo "<br>"; echo "<hr>";
+    echo "Apellidos: " $apellidos;echo "<br>"; echo "<hr>";
+    echo "Sexo: " $sexo;echo "<br>"; echo "<hr>";
+    echo "Correo: " $correo;echo "<br>"; echo "<hr>";
+    echo "Poblaciòn: " $poblacion;echo "<br>"; echo "<hr>";
+    
+?>
+
